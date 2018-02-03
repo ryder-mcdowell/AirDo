d403c2a420b07a5b5074f23f60598fa20f5dcefb
Dylan Drudge <drudged@oregonstate.edu>
2018-02-03 11:21:20 -0800

    Add dependencies to all files and much more
    add menu and nav bar and JS to go with them.

---
+++ JavaScript/index.js
@@ -0,0 +1,30 @@
+new Vue({
+  el: '#root',
+})
+
+
+new Vue({
+  el: '#menuz',
+
+  data: {
+      isActiveClass :"'is-active'",
+      isLoading   : false,
+      menuIsActive : false,
+      login : 'Login'
+  },
+  methods: {
+
+    activateMenu(){
+      this.menuIsActive = !this.menuIsActive;
+      this.isLoading = ! this.isLoading;
+      if (this.menuIsActive === true) {
+        this.login = ''
+      }
+      else {
+        this.login = 'Login'
+      }
+    }
+
+  }
+
+});
